8ca6a9146eeda58adc36b40523aa6e123217fbf9
Alexey Ivanov <alexioi1993@gmail.com>
2020-10-06 15:36:00 +0300

    add dropdown

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -1,39 +1,14 @@
-// const dropdown_hiden = document.getElementsByClassName("dropdown_hiden")[0];
-
-// let selectedButton
-
-// function changeVisibility(button) {
-//     selectedButton = button;
-//     if (dropdown_hiden.classList.contains('dropdown_hiden')) {
-//         dropdown_hiden.classList.remove('dropdown_hiden')
-//     } else {
-//         dropdown_hiden.classList.add('dropdown_hiden')
-//     }
-// }
-
-// document.onclick = function(event) {
-//     let target = event.target;
-
-//     if (target.classList.contains('test')) {
-//         changeVisibility(target)
-//     }
-// };
-
 const dropdownButton = document.getElementsByClassName("dropdown__button-visible")[0]
 const dropdownVisible = document.getElementsByClassName("dropdown__visible")[0]
-
-const li1 = document.getElementsByClassName("dropdown__item")[0]
-const li2 = document.getElementsByClassName("dropdown__item")[1]
-const li3 = document.getElementsByClassName("dropdown__item")[2]
 
 dropdownButton.onclick = function() {
     let target = event.target;
 
-    if (dropdownVisible.classList.contains('hide')) {
-        dropdownVisible.classList.add('show');
-        dropdownVisible.classList.remove('hide');
+    if (dropdownVisible.classList.contains('dropdown__visible_hide')) {
+        dropdownVisible.classList.add('dropdown__visible_show');
+        dropdownVisible.classList.remove('dropdown__visible_hide');
     } else {
-        dropdownVisible.classList.add('hide');
-        dropdownVisible.classList.remove('show');
+        dropdownVisible.classList.add('dropdown__visible_hide');
+        dropdownVisible.classList.remove('dropdown__visible_show');
     }
 }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import './main.sass';
 
 import './components/dropdown/dropdown'
+import './components/rangeslider/rangeslider'
 
 import smile from './svg/smile.svg'
 import home from './svg/home.svg'
