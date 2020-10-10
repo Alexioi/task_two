c982c4f9f8d59ee9954e6e1e8871535725117270
Alexey Ivanov <alexioi1993@gmail.com>
2020-10-10 13:01:39 +0300

    refactoring colors and type

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
-import './main.sass';
+import './main.sass'
 
-import './components/dropdown/dropdown'
-import './components/rangeslider/rangeslider'
+// import jQuery from 'jquery';
+
+// import './components/dropdown/dropdown'
+// import './components/rangeslider/rangeslider'
+// import './components/calendar/calendar'
 
 import smile from './svg/smile.svg'
 import home from './svg/home.svg'
