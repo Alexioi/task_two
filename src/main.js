f7e33809c06fac291332fefa67aa6c3ed008b41a
Alexey Ivanov <alexioi1993@gmail.com>
2020-10-11 13:52:24 +0300

    ad header

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import './main.sass'
 
 // import jQuery from 'jquery';
 
-// import './components/dropdown/dropdown'
+import './components/dropdown/dropdown'
 // import './components/rangeslider/rangeslider'
 // import './components/calendar/calendar'
 
 import smile from './svg/smile.svg'
 import home from './svg/home.svg'
+import full_logo from './components/logo/img/full_logo.svg'
