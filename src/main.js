52fc1a75c4603ade30b44d69cf139e9a082ac7f9
Alexey Ivanov <alexioi1993@gmail.com>
2020-11-07 18:25:01 +0300

    refactoring radio btn

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
-import './main.sass'
+import "./main.sass";
 
-import './components/dropdown/dropdown'
-import './components/checkbox_list/checkbox_list'
-import './components/pagination/pagination'
-import './components/like-button/like-button'
-import './components/comment/comment'
-import './components/caption/caption'
-import './components/text-field/text-field'
-import './components/button/button'
-import './components/login/login'
-import './components/toggle/toggle'
+import "./components/dropdown/dropdown";
+import "./components/checkbox_list/checkbox_list";
+import "./components/pagination/pagination";
+import "./components/like-button/like-button";
+import "./components/comment/comment";
+import "./components/caption/caption";
+import "./components/text-field/text-field";
+import "./components/button/button";
+import "./components/login/login";
+import "./components/toggle/toggle";
+import "./components/radio-buttons/radio-buttons";
 
-import './pages/form_elements/form-elements'
-
+import "./pages/form_elements/form-elements";
--- webpack.config.js
+++ webpack.config.js
@@ -1,92 +1,92 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    entry: {
-        main: './src/main.js',
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js'
-    },
+  entry: {
+    main: "./src/main.js",
+  },
 
-    module: {
-        rules: [{
-            test: /\.pug$/,
-            use: 'pug-loader'
-        }, {
-            test: /\.sass$/,
-            use: [
-                MiniCssExtractPlugin.loader, 
-                'css-loader',
-                'sass-loader',
-            ],
-        }, {
-            test: /\.(woff|woff2|eot|ttf|otf)$/,
-            use: [{
-                loader: 'file-loader',
-                options: {
-                    name: './font/[name].[ext]',
-                },
-            }],
-        }, 
-        {
-            test: /\.(png|jpe?g|gif|svg|ico)$/i,
-            use: [{
-                loader: 'file-loader',
-                options: {
-                    name: './img/[name].[ext]',
-                },
-            }, ],
-        }, 
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        use: "pug-loader",
+      },
+      {
+        test: /\.sass$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "./font/[name].[ext]",
+            },
+          },
         ],
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 9001
-    },
-    plugins: [
-        new CopyPlugin({
-            patterns: [
-                {
-                    from: 'src/**/*.png',
-                    to: 'img',
-                    flatten: true, 
-                },
-                {
-                    from: 'src/**/*.svg',
-                    to: 'img',
-                    flatten: true, 
-                },
-            ],
-        }),
-        new MiniCssExtractPlugin(),
-        new HtmlWebpackPlugin({
-            template: './src/pages/index/index.pug',
-            filename: 'index.html'
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/pages/colors_and_type/colors_and_type.pug',
-            filename: 'colors_and_type.html'
-
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/pages/form_elements/form_elements.pug',
-            filename: 'form_elements.html'
-
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/pages/headers_footers/headers_footers.pug',
-            filename: 'headers_footers.html'
-
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/pages/cards/cards.pug',
-            filename: 'cards.html'
-
-        }),
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|ico)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "./img/[name].[ext]",
+            },
+          },
+        ],
+      },
     ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "src/**/*.png",
+          to: "img",
+          flatten: true,
+        },
+        {
+          from: "src/**/*.svg",
+          to: "img",
+          flatten: true,
+        },
+      ],
+    }),
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/index/index.pug",
+      filename: "index.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/colors_and_type/colors_and_type.pug",
+      filename: "colors_and_type.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/form_elements/form_elements.pug",
+      filename: "form_elements.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/headers_footers/headers_footers.pug",
+      filename: "headers_footers.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/cards/cards.pug",
+      filename: "cards.html",
+    }),
+  ],
 };
