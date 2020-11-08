142e2c802ecd25c12fc6b39bac547f44a79f9fd6
Alexey Ivanov <alexioi1993@gmail.com>
2020-11-08 11:15:21 +0300

    refactoring pagination

--- src/components/pagination/pagination.js
+++ src/components/pagination/pagination.js
@@ -2,16 +2,33 @@ import "./pagination.sass";
 
 const paginationList = document.querySelectorAll(".pagination__item");
 
-let spanl = document.createElement("span");
-spanl.className = "pagination__dots";
-spanl.innerHTML = "...";
+let dotsLeft = document.createElement("span");
+dotsLeft.className = "pagination__dots";
+dotsLeft.innerHTML = "...";
 
-let spanr = document.createElement("span");
-spanr.className = "pagination__dots";
-spanr.innerHTML = "...";
+let dotsRight = document.createElement("span");
+dotsRight.className = "pagination__dots";
+dotsRight.innerHTML = "...";
+
+let btnLast = document.createElement("button");
+btnLast.className = "pagination__next-btn";
+btnLast.innerHTML = "Last";
+btnLast.onclick = function() {
+  let target = document.querySelector(".pagination__item_focus").innerText - 1;
+  hidePaginationItems(target);
+} 
+
+let btnNext = document.createElement("button");
+btnNext.className = "pagination__next-btn";
+btnNext.innerHTML = "Next";
+btnNext.onclick = function () {
+  let target = Number(document.querySelector(".pagination__item_focus").innerText) + 1;
+  hidePaginationItems(target);
+}; 
 
 function hidePaginationItems(target) {
   let itemsVisible = [];
+
   for (let i = 2; i < paginationList.length; i++) {
     if (
       i != target &&
@@ -25,29 +42,42 @@ function hidePaginationItems(target) {
   }
 
   if (itemsVisible[itemsVisible.length - 1] == 14) {
-    paginationList[paginationList.length - 1].before(spanr);
+    paginationList[paginationList.length - 1].before(dotsRight);
   } else {
-    spanr.remove();
+    dotsRight.remove();
   }
 
   if (itemsVisible[0] == 2) {
-    paginationList[0].after(spanl);
+    paginationList[0].after(dotsLeft);
   } else {
-    spanl.remove();
+    dotsLeft.remove();
   }
 
   for (let i = 0; i < paginationList.length; i++) {
     paginationList[i].classList.remove("pagination__item_hiden");
+    paginationList[i].classList.remove("pagination__item_focus");
   }
 
+  paginationList[target - 1].classList.add("pagination__item_focus");
+
   for (let j of itemsVisible) {
-    paginationList[j - 1].classList.toggle("pagination__item_hiden");
+    paginationList[j - 1].classList.add("pagination__item_hiden");
   }
-  console.log(itemsVisible);
+
+  if (target != 1) {
+    paginationList[0].before(btnLast);
+  } else {
+    btnLast.remove();
+  }
+
+  if (target != 15) {
+    paginationList[14].after(btnNext);
+  } else {
+    btnNext.remove();
+  }
 }
 
 if (paginationList.length > 0) {
-  console.log(paginationList.length);
   window.onload = function () {
     let target = paginationList[0].innerText;
     hidePaginationItems(target);
