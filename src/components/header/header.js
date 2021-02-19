591130b5e8ed3c0cf442867ebdd3a9ec74a2d974
Alexey <alexioi1993@gmail.com>
2021-02-19 10:43:33 +0300

    refactoring header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,1 +1,10 @@
 import "./header.scss";
+
+function toggleMenu() {
+	headerNav.style.display = headerNav.style.display !== 'flex' ? 'flex' : 'none';
+}
+
+const burgerButton = document.querySelector('.js-header__burger-button');
+const headerNav = document.querySelector('.js-header__nav');
+
+burgerButton.addEventListener('click', toggleMenu);
